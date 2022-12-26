3038b3d438cf0325400587283ca5266f2397abb5
Ali_Asl <48028559+Aslanbayli@users.noreply.github.com>
2022-12-26 19:59:21 +0400

    card styles

--- frontend/src/components/CountryCard.tsx
+++ frontend/src/components/CountryCard.tsx
@@ -2,9 +2,16 @@ import React from "react";
 
 function CountryCard({country}: any) {
     return (
-        <li className="m-[40px] bg-white text-black" key={country.cca2}>
-            <img src={country.flags.png} alt="flag" />
-            <p>{country.name.common}</p>
+        <li key={country.cca2}>
+            <div className="m-[40px] bg-white text-black rounded-md shadow-[0px_2px_4px_0px_rgba(0,0,0,0.2)] pb-5 ">
+                <img src={country.flags.png} alt="flag" className="rounded-t-md w-[100%] h-[100%] object-cover"/>
+                <div className="py-7 pl-7 font-nunito font-[300]">
+                    <p className="py-2 text-lg font-[900]">{country.name.common}</p>
+                    <p><span className="font-[600]">Population: </span>{country.population || "unknown"}</p>
+                    <p><span className="font-[600]">Region: </span>{country.region || "unknown"}</p>
+                    <p><span className="font-[600]">Capital: </span>{country.hasOwnProperty("capital") ? country.capital[0] : "unknown" }</p> 
+                </div>
+            </div>
         </li>
     );
 }
